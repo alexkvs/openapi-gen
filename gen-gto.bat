fd46fd0352575827b2cda53c08b2466f0f0aa5f8
Aleksandr Kivshar <39238732+alexkvs@users.noreply.github.com>
2021-09-30 10:56:27 +0300

    Update gen-gto.bat

--- gen-gto.bat
+++ gen-gto.bat
@@ -1,7 +1,1 @@
 java -jar C:\Users\alexkvs\.m2\repository\org\openapitools\openapi-generator-cli\5.0.1\openapi-generator-cli-5.0.1.jar generate -i schema.json -o ./src --api-package ru.alexkvs.project_name.api --model-package ru.alexkvs.project_name.dto --invoker-package ru.alexkvs.project_name.client --group-id ru.alexkvs.project_name --artifact-id ru.alexkvs.project_name --artifact-version 0.0.1-SNAPSHOT --generator-name java --library RestTemplate --additional-properties java8=true, useBeanValidation=true, performBeanValidation=true
-   
-   
-   
-   
-   
-   
